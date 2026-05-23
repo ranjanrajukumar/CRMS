9867f3748597c1b16ec34429b4956b5c7933a96c
ranjanrajukumar <ranjanrajukumar4@gmail.com>
2026-05-23 19:16:51 +0530

    aa

--- src/features/dashboard/dashboardService.js
+++ src/features/dashboard/dashboardService.js
@@ -1,19 +1,45 @@
+// import axios from "axios";
+// import { getAuthToken } from "../auth/authUtils";
+
+// export const getProcessDashboard = async ({ userType, userName }) => {
+//   const payload = { userType, userName };
+//   const token = getAuthToken();
+
+//   const response = await axios.get("api/ManageBankDashboard/process/dashboard", {
+//     params: payload,
+//     data: payload,
+//     headers: {
+//       Accept: "*/*",
+//       "Content-Type": "application/json",
+//       ...(token ? { Authorization: `Bearer ${token}` } : {}),
+//     },
+//   });
+
+//   return response.data;
+// };
+
 import axios from "axios";
 import { getAuthToken } from "../auth/authUtils";
 
 export const getProcessDashboard = async ({ userType, userName }) => {
-  const payload = { userType, userName };
   const token = getAuthToken();
 
-  const response = await axios.get("/api/ManageBankDashboard/process/dashboard", {
-    params: payload,
-    data: payload,
-    headers: {
-      Accept: "*/*",
-      "Content-Type": "application/json",
-      ...(token ? { Authorization: `Bearer ${token}` } : {}),
-    },
-  });
+  const response = await axios.get(
+    "/api/ManageBankDashboard/process/dashboard",
+    {
+      params: {
+        userType,
+        userName,
+      },
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+    }
+  );
 
   return response.data;
 };
